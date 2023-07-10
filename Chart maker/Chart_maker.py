f68f1aad06dbaf99546da0f3c2c88d3203ad7439
Varad Shinde <99754991+Admsher@users.noreply.github.com>
2023-07-10 12:43:39 +0530

    Last Commit

--- Chart maker/Chart_maker.py
+++ Chart maker/Chart_maker.py
@@ -6,11 +6,12 @@ from tkinter import *
 from openpyxl import load_workbook
 from tkinter import messagebox
 import os
+import pandas as pd
 
 
 
 Filename='C:/BITS/PS 1/New folder/Tests.xlsx'
-excel_chart=load_workbook(Filename)
+excel_chart=load_workbook(Filename,data_only=True, keep_vba=True)
 schedule_sheet=excel_chart[excel_chart.sheetnames[0]]
 symptom_sheet=excel_chart[excel_chart.sheetnames[1]]
 expected_date=1
@@ -30,12 +31,11 @@ for i in range(0,len(schedule_sheet['A'])):
 ###Window 1 func.###
 
 def chart_calculator():
-
+      
         day_date=int(day.get())
         month_date=int(month.get())
         year_date=int(year.get())   
         expected_date= datetime.date(year_date,month_date,day_date)
-        print(varPIH.get())
         if(varPIH.get()=="True"):
             for i in range(0,41):    
                 Description[i]=str(schedule_sheet.cell(row=(i+1),column=2).value)+","+str(schedule_sheet.cell(row=(i+2),column=3).value)
@@ -100,35 +100,44 @@ def confirm():
 def Window2():
    ###Window 2 func.###  
      def result():
+      
       symptom_prediction=[]
-
+      Answersymptom1=tkinter.Label(frame2,text=SymptomVar1.get())
+      Answersymptom1.grid(row=2,column=2)
+      Answersymptom2=tkinter.Label(frame2,text=SymptomVar2.get())
+      Answersymptom2.grid(row=3,column=2) 
+      Answersymptom3=tkinter.Label(frame2,text=SymptomVar3.get())
+      Answersymptom3.grid(row=4,column=2) 
+      
+    
       for i in range(2,5): 
-        for j in range(3,7):
+        for j in range(3,9):
           if(SymptomVar1.get()==symptom_sheet.cell(row=i,column=j).value):
-              Answersymptom1=tkinter.Label(frame2,text=symptom_sheet.cell(row=i,column=j).value)
-              Answersymptom1.grid(row=2,column=2)
               symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)
               
               
                     
       for i in range(2,5): 
-        for j in range(3,7):
+        for j in range(3,9):
           if(SymptomVar2.get()==symptom_sheet.cell(row=i,column=j).value):
-              Answersymptom2=tkinter.Label(frame2,text=symptom_sheet.cell(row=i,column=j).value)
-              Answersymptom2.grid(row=3,column=2) 
               symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)
               
                     
       for i in range(2,5): 
-        for j in range(3,7):
+        for j in range(3,9):
           if(SymptomVar3.get()==symptom_sheet.cell(row=i,column=j).value):
-               Answersymptom3=tkinter.Label(frame2,text=symptom_sheet.cell(row=i,column=j).value)
-               Answersymptom3.grid(row=4,column=2) 
                symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)
-                 
-                   
-   
- 
+      
+      for i in range(2,5): 
+          if(int(SystoleEntry.get())== int(symptom_sheet.cell(row=i,column=10).value)):
+               symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)
+      for i in range(2,5): 
+          if(int(DiastoleLabelEntry.get())== int((symptom_sheet.cell(row=i,column=11).value))):
+               symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)
+      for i in range(2,5): 
+          if(int(WeightEntry.get())== int((symptom_sheet.cell(row=i,column=12).value))):
+               symptom_prediction.append(symptom_sheet.cell(row=i,column=3).value)                   
+                    
       Result=tkinter.Label(frame2,text=max(set(symptom_prediction),key=symptom_prediction.count),font='bold')
       Result.grid(row=8,column=3)
 
@@ -150,7 +159,7 @@ def Window2():
      Symptom2Label=tkinter.Label(frame2,text="Symptom2")
      Symptom2Label.grid(row=3,column=0)
      SymptomVar2=StringVar()
-     SymptomVar2.set("100")
+     SymptomVar2.set("Select")
      Symptom2Entry=tkinter.OptionMenu(frame2,SymptomVar2,*symptom_list)
      Symptom2Entry.grid(row=3,column=1)
 
@@ -167,16 +176,19 @@ def Window2():
      SystoleLabel.grid(row=5,column=0)
      SystoleEntry=tkinter.Entry(frame2)
      SystoleEntry.grid(row=5,column=1)
+     SystoleEntry.insert(0,"0")
      
      DiastoleLabel=tkinter.Label(frame2,text="Diastole")
      DiastoleLabel.grid(row=6,column=0)
      DiastoleLabelEntry=tkinter.Entry(frame2)
      DiastoleLabelEntry.grid(row=6,column=1)
+     DiastoleLabelEntry.insert(0,"0")
      
      WeightIncrease=tkinter.Label(frame2,text="Weight Increase")
      WeightIncrease.grid(row=7,column=0)
      WeightEntry=tkinter.Entry(frame2)
      WeightEntry.grid(row=7,column=1)
+     WeightEntry.insert(0,"0")
      
      Predict=tkinter.Button(frame2,text="Predict",command=result)
      Predict.grid(row=7,column=2)
